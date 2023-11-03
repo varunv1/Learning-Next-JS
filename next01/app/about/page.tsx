9d6b3c8a02260e769c6d41212cc970a55526d5d9
Varun <varunv1a2r3@gmail.com>
2023-11-03 20:22:05 +0530

    Added new Page and router, worked with error, loading pages and added new style

---
+++ next01/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+export default function About() {
+    // throw new Error('ehhe')
+    return (
+        <>
+            <h1>page</h1>
+            <Link href="/">Link to homepage</Link>
+        </>
+    )
+}
